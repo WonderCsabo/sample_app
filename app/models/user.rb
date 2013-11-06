e8be8acda7e8a1dda76312634bb3ed75e0448ca8
WonderCsabo <kozakcsabi@gmail.com>
2013-11-06 13:42:40 +0100

    User depentent_destroy its Microposts, also changed  default_scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   has_secure_password
-  has_many :microposts
+  has_many :microposts, dependent: :destroy
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
