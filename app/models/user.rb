7af1acbdf73570aa410df2b872a2dd00d897dbc1
WonderCsabo <kozakcsabi@gmail.com>
2013-11-06 13:26:50 +0100

    Added User-Micropost has_many association

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ActiveRecord::Base
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }
   has_secure_password
+  has_many :microposts
 
   def User.new_remember_token
     SecureRandom.urlsafe_base64
